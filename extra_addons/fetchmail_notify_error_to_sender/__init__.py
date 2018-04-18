117e0c0e625ba45573c11ab10b6940ae616e3209
HaiDD <haidd.uit@gmail.com>
2018-04-18 18:23:44 +0700

    [MIG] Module migration: fetchmail_notify_error_to_sender

---
+++ extra_addons/fetchmail_notify_error_to_sender/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# Copyright 2017 Eficent <http://www.eficent.com>
+# Copyright 2018 Hai Dinh <http://www.haidd.xyz>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
